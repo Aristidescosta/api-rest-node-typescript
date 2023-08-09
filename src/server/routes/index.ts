7e733550ab9a60725abb40f71b15cf6c860f59e2
Aristidescosta <aristidiscosta200@gmail.com>
2023-08-09 11:01:23 -0700

    feat: add city controller

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,6 +1,13 @@
 import { Router } from "express";
+import { CidadeController } from "../controllers/index";
 
 const router = Router();
 
+
+router.get("/", (_, res) =>{
+  res.send("Olá mundo");
+});
+
+router.post("/cidades", CidadeController.create);
 
 export { router };
