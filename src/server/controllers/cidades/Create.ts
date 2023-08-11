3cfe5b92bcd4d4607e4622a5175a1ec3b03b22a0
Aristidescosta <aristidiscosta200@gmail.com>
2023-08-11 06:32:09 -0700

    refeat: adding field validations with YUP

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,12 +1,31 @@
 import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import * as yup from "yup";
 
 interface  ICidade{
   nome: string;
+  estado: string;
 }
 
+const bodyValidation: yup.Schema<ICidade> = yup.object().shape({
+  nome: yup.string().required().min(3),
+  estado: yup.string().required().min(3),
+});
 
-export const create = (req: Request<{}, {}, ICidade>, res: Response) => {
+export const create = async(req: Request<{}, {}, ICidade>, res: Response) => {
+  let validateData: ICidade | undefined = undefined;
 
+
+  try {
+    validateData = await bodyValidation.validate(req.body);
+  } catch (error) {
+    const yupError = error as yup.ValidationError;
+    return res.status(StatusCodes.BAD_REQUEST).json({
+      errors: {
+        default: yupError.message
+      }
+    });
+  }
   console.log(req.body.nome);
 
   return res.send("Create");
