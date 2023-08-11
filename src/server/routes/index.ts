f1d4ff314193c402838022301b44eed2ac3c6788
Aristidescosta <aristidiscosta200@gmail.com>
2023-08-11 18:09:16 +0100

    feat: add method of GetAll

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -10,6 +10,7 @@ router.get("/", (_, res) =>{
   res.send("Olá mundo");
 });
 
+router.get("/cidades", CidadeController.getAllValidation, CidadeController.getAll);
 router.post("/cidades", CidadeController.createValidation, CidadeController.create);
 
 export { router };
