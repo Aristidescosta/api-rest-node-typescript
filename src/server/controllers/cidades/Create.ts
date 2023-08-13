bebe5a4f0683dc0db5a6a513e1779c635fd6cd2d
Aristidescosta <aristidiscosta200@gmail.com>
2023-08-13 15:02:03 +0100

    feat: Adding all tests for cities controller

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -15,8 +15,5 @@ export const createValidation = validation((getSchema) => ({
 }));
 
 export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
-
-  console.log(req.body, req.query);
-
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send("Não implementado!");
+  return res.status(StatusCodes.CREATED).json(1);
 };
--- src/server/controllers/cidades/GetById.ts
+++ src/server/controllers/cidades/GetById.ts
@@ -3,19 +3,26 @@ import { Request, Response } from "express";
 import * as yup from "yup";
 
 import { validation } from "../../shared/middleware";
-interface IQueryProps {
+interface IParamProps {
   id?: number;
 }
 
 export const getBuyIdValidation = validation((getSchema) => ({
-  params: getSchema<IQueryProps>(yup.object().shape({
+  params: getSchema<IParamProps>(yup.object().shape({
     id: yup.number().integer().optional().moreThan(0),
   }))
 }));
 
-export const getBuyId = async (req: Request<{}, {}, {}, IQueryProps>, res: Response) => {
+export const getBuyId = async (req: Request<IParamProps>, res: Response) => {
 
-  console.log(req.params);
+  if(Number(req.params.id) === 99999) return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+    errors:{
+      default: "Registo não encontrado"
+    }
+  });
 
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send("Não implementado!");
+  return res.status(StatusCodes.OK).json({
+    id: req.params.id,
+    nome: "Luanda"
+  });
 };
